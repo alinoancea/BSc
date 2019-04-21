51be43e05bdda4fd4e1af5f181053c4e43edd760
Alin Oancea <oanceaalinandrei@gmail.com>
2019-04-21 18:09:15 +0300

    add first version of ProcessWatcher

--- tools/common/processtool.py
+++ tools/common/processtool.py
@@ -1,4 +1,4 @@
-from ctypes import c_int, c_bool, c_void_p, c_long, c_short, c_ulong, c_char, c_ushort, c_ubyte
+from ctypes import c_int, c_bool, c_void_p, c_long, c_short, c_ulong, c_char, c_ushort, c_ubyte, c_long
 from ctypes import windll, byref, Structure, sizeof, POINTER
 
 
@@ -9,10 +9,26 @@ DWORD = c_ulong
 WORD = c_ushort
 LPTSTR = POINTER(c_char)
 LPBYTE = POINTER(c_ubyte)
+LONG = c_long
+CHAR = c_char
+if sizeof(c_void_p) == 8:
+    from ctypes import c_longlong
+    ULONG_PTR = c_longlong
+else:
+    ULONG_PTR = c_long
 
 
 CREATE_NEW_CONSOLE = 0x00000010
 CREATE_SUSPENDED = 0x00000004
+
+TH32CS_SNAPPROCESS = 0x00000002
+TH32CS_SNAPTHREAD = 0x00000004
+THREAD_SUSPEND_RESUME = 0x0002
+
+MAX_PATH = 0x00000104
+INVALID_HANDLE_VALUE = -1
+ERROR_NO_MORE_FILES = 0x12
+
 
 
 ### Windows structures
@@ -50,12 +66,49 @@ class PROCESS_INFORMATION(Structure):
     ]
 
 
+
+class PROCESSENTRY32(Structure):
+    _fields_ = [
+        ('dwSize',              DWORD),
+        ('cntUsage',            DWORD),
+        ('th32ProcessID',       DWORD),
+        ('th32DefaultHeapID',   ULONG_PTR),
+        ('th32ModuleID',        DWORD),
+        ('cntThreads',          DWORD),
+        ('th32ParentProcessID', DWORD),
+        ('pcPriClassBase',      LONG),
+        ('dwFlags',             DWORD),
+        ('szExeFile',           CHAR * MAX_PATH)
+    ]
+
+
+
+class THREADENTRY32(Structure):
+    _fields_ = [
+        ('dwSize',              DWORD),
+        ('cntUsage',            DWORD),
+        ('th32ThreadID',        DWORD),
+        ('th32OwnerProcessID',  DWORD),
+        ('tpBasePri',           LONG),
+        ('tpDeltaPri',          LONG),
+        ('dwFlags',             DWORD)
+    ]
+
+
+
 ### Windows function headers
 
 CreateProcess = windll.kernel32.CreateProcessW
 GetLastError = windll.kernel32.GetLastError
 ResumeThread = windll.kernel32.ResumeThread
 SuspendThread = windll.kernel32.SuspendThread
+CreateToolhelp32Snapshot = windll.kernel32.CreateToolhelp32Snapshot
+Process32First = windll.kernel32.Process32First
+Process32Next = windll.kernel32.Process32Next
+CloseHandle = windll.kernel32.CloseHandle
+Thread32First = windll.kernel32.Thread32First
+Thread32Next = windll.kernel32.Thread32Next
+OpenThread = windll.kernel32.OpenThread
 TerminateProcess = windll.kernel32.TerminateProcess
 
 
@@ -148,5 +201,88 @@ class ProcessCreator():
             return (1, GetLastError())
         return (0, ret_code)
 
+
+
+class ProcessWatcher():
+
+
+    def __init__(self):
+        self.p = []
+
+
+    def snap(self):
+        hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0x0)
+        if hProcessSnap == INVALID_HANDLE_VALUE:
+            print('   [!] ERROR-snap: CreateToolhelp32Snapshot. Code: %d' % (GetLastError(),))
+            return
+
+        pe32 = PROCESSENTRY32()
+        pe32.dwSize = sizeof(pe32)
+        if not Process32First(hProcessSnap, byref(pe32)):
+            print('   [!] ERROR-snap: Process32First. Code: %d' % (GetLastError(),))
+            CloseHandle(hProcessSnap)
+            return
+
+        while 1:
+            if pe32.th32ProcessID not in self.p:
+                self.p.append(pe32.th32ProcessID)
+
+            code = Process32Next(hProcessSnap, byref(pe32))
+            if not code:
+                break
+
+        CloseHandle(hProcessSnap)
+
+
+    def suspend_differences(self):
+        hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0x0)
+        if hProcessSnap == INVALID_HANDLE_VALUE:
+            print('   [!] ERROR-suspend: CreateToolhelp32Snapshot. Code: %d' % (GetLastError(),))
+            return
+
+        pe32 = PROCESSENTRY32()
+        pe32.dwSize = sizeof(pe32)
+        if not Process32First(hProcessSnap, byref(pe32)):
+            print('   [!] ERROR-suspend: Process32First. Code: %d' % (GetLastError(),))
+            CloseHandle(hProcessSnap)
+            return
+
+        to_suspend = []
+        while 1:
+            if pe32.th32ProcessID not in self.p:
+                to_suspend.append(pe32.th32ProcessID)
+
+            code = Process32Next(hProcessSnap, byref(pe32))
+            if not code:
+                break
+
+        CloseHandle(hProcessSnap)
+
+        self.suspend_process(to_suspend)
+
+
+    def suspend_process(self, pid):
+        hThreadSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0x0)
+
+        te32 = THREADENTRY32()
+        te32.dwSize = sizeof(te32)
+        if not Thread32First(hThreadSnapshot, byref(te32)):
+            print('   [!] ERROR-suspend thread: Thread32First. Code: %d' % (GetLastError(),))
+            CloseHandle(hThreadSnapshot)
+            return
+
+        while 1:
+            if te32.th32OwnerProcessID in pid:
+                hThread = OpenThread(THREAD_SUSPEND_RESUME, None, te32.th32ThreadID)
+
+                if hThread:
+                    SuspendThread(hThread)
+                    CloseHandle(hThread)
+
+            code = Thread32Next(hThreadSnapshot, byref(te32))
+            if not code:
+                break
+
+        CloseHandle(hThreadSnapshot)
 
 
