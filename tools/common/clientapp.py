d36ecdefe919dd7cd55c65152735455e6509a247
Alin Oancea <oanceaalinandrei@gmail.com>
2019-04-21 18:14:00 +0300

    rewrite clientapp to work with process and registry modules

--- tools/common/clientapp.py
+++ tools/common/clientapp.py
@@ -6,8 +6,9 @@ import zipfile
 import subprocess
 import sys
 
-from ctypes import c_int, c_bool, c_void_p, c_long, c_short, c_ulong, c_char, c_ushort, c_ubyte
-from ctypes import windll, byref, Structure, sizeof, POINTER
+from processtool import ProcessCreator, ProcessWatcher
+from processtool import CREATE_SUSPENDED
+import registrytool
 
 
 DEPLOY_DIR = os.path.join('C:\\', 'maltest')
@@ -25,99 +26,42 @@ logger = open(CLIENT_LOG_FN, 'w')
 sys.stdout = sys.stderr = logger
 
 
-### START - Windows constants
-
-HANDLE = c_void_p
-DWORD = c_ulong
-WORD = c_ushort
-LPTSTR = POINTER(c_char)
-LPBYTE = POINTER(c_ubyte)
-
-### END - Windows constants
-
-
-### START - Windows structures
-
-class STARTUPINFO(Structure):
-    _fields_ = [
-        ('cb',              DWORD),
-        ('lpReserved',      LPTSTR),
-        ('lpDesktop',       LPTSTR),
-        ('pTitle',          LPTSTR),
-        ('dwX',             DWORD),
-        ('dwY',             DWORD),
-        ('dwXSize',         DWORD),
-        ('dwYSize',         DWORD),
-        ('dwXCountChars',   DWORD),
-        ('dwYCountChars',   DWORD),
-        ('dwFillAttribute', DWORD),
-        ('dwFlags',         DWORD),
-        ('wShowWindow',     WORD),
-        ('cbReserved2',     WORD),
-        ('lpReserved2',     LPBYTE),
-        ('hStdInput',       HANDLE),
-        ('hStdOutput',      HANDLE),
-        ('hStdError',       HANDLE)
-    ]
-
-
-
-class PROCESS_INFORMATION(Structure):
-    _fields_ = [
-        ('hProcess',    HANDLE),
-        ('hThread',     HANDLE),
-        ('dwProcessId', DWORD),
-        ('dwThreadId',  DWORD)
-    ]
-
-# END - Windows structures
-
-
-# START - Windows function headers
-
-CreateProcess = windll.kernel32.CreateProcessW
-GetLastError = windll.kernel32.GetLastError
-ResumeThread = windll.kernel32.ResumeThread
-
-# END - Windows function headers
-
 # start monitoring apps
 
 #start a suspended hollows_hunter.exe and after WAIT_TIME resume it
-hh_pi = PROCESS_INFORMATION()
-hh_si = STARTUPINFO()
-hh_si.cb = sizeof(hh_si)
 # protect needed imports for hollows_hunter
 #TODO: rewrite this crap
 dll_lock = open('%s\\tools\\pe-sieve.dll' % (DEPLOY_DIR,))
-hollows_hunter_path = '%s\\tools\\hollows_hunter.exe /kill' % (DEPLOY_DIR,)
-ret = CreateProcess(None, hollows_hunter_path, None, None, None, 0x4, None, EXTRACTION_DIR, byref(hh_si),
-        byref(hh_pi))
+hollows_hunter_path = '%s\\tools\\hollows_hunter.exe' % (DEPLOY_DIR,)
 
-pi = PROCESS_INFORMATION()
-si = STARTUPINFO()
-si.cb = sizeof(si)
+hollows_proc = ProcessCreator()
+response = hollows_proc.create(hollows_hunter_path, creation_flags=CREATE_SUSPENDED, working_dir=EXTRACTION_DIR)
 
-logger.write('[#] Executing [%s]\n' % (MALWARE_PATH,))
-ret = CreateProcess(MALWARE_PATH, None, None, None, None, 0x0, None, None, byref(si), byref(pi))
+if response[0]:
+    print('[!] ERROR: Hollows hunter process creation. Code: %d' % (response[1],))
+    sys.exit()
 
-if ret:
-    logger.write('CreateProcess success! ID: %d\n' % (pi.dwProcessId,))
-else:
-    err = GetLastError()
-    logger.write('Something went wrong %x!\n' % (err,))
-    sys.exit(err)
+registrytool.create_snapshot()
+process_watcher = ProcessWatcher()
+process_watcher.snap()
+
+print('[#] Executing [%s]...' % (MALWARE_PATH,))
+malware_proc = ProcessCreator()
+response = malware_proc.create(MALWARE_PATH)
+
+if response[0]:
+    print('[!] ERROR: Malware process creation. Code: %s' % (response[1],))
+    sys.exit()
+
+print('[#] Malware process successfully created. PID: %d' % (malware_proc.pid,))
 
 time.sleep(WAIT_TIME)
-ResumeThread(hh_pi.hThread)
-time.sleep(WAIT_TIME)
+hollows_proc.resume()
 
-# logger.write('[#] Dumping PID [%d] using procdump.exe\n' % (pi.dwProcessId,))
-# subprocess.Popen(['%s\\tools\\procdump.exe' % (DEPLOY_DIR,), '-t', '-ma', str(pi.dwProcessId), '/AcceptEula'],
-#         cwd=EXTRACTION_DIR)
+process_watcher.suspend_differences()
+registrytool.show_diff()
 
-# in case hollows_hunter doesn't kill the process
-subprocess.run(['taskkill', '/F', '/PID', str(pi.dwProcessId)])
+malware_proc.terminate()
 
 logger.close()
 dll_lock.close()
